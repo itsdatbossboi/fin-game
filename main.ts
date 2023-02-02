d1c328e90207aeb6651d077d78b07e44adf7bc6b
itsdatbossboi <76117839+itsdatbossboi@users.noreply.github.com>
2023-02-01 20:09:08 -0700

    fixed a mine button bug and created a second gem sprite

--- main.ts
+++ main.ts
@@ -7,18 +7,22 @@ namespace StatusBarKind {
     export const ItemQuantity = StatusBarKind.create()
     export const Cooldown = StatusBarKind.create()
 }
-let list = [
-0,
-1,
-1,
-0,
-0,
-2,
-0,
-0,
-3,
-4
-]
+let list: number[] = []
+for (let index = 0; index < 30; index++) {
+    list.push(0)
+}
+for (let index = 0; index < 20; index++) {
+    list.push(1)
+}
+for (let index = 0; index < 10; index++) {
+    list.push(2)
+}
+for (let index = 0; index < 5; index++) {
+    list.push(3)
+}
+for (let index = 0; index < 5; index++) {
+    list.push(4)
+}
 tiles.setCurrentTilemap(tilemap`level1`)
 let mine_button = sprites.create(img`
     ................................
@@ -76,6 +80,8 @@ let mySprite = sprites.create(img`
 controller.moveSprite(mySprite)
 let mySprite2 = sprites.create(assets.image`redgem`, SpriteKind.icon)
 mySprite2.setPosition(60, 10)
+let mySprite3 = sprites.create(assets.image`bluegem`, SpriteKind.icon)
+mySprite3.setPosition(100, 10)
 let statusbar = statusbars.create(32, 3, StatusBarKind.Cooldown)
 statusbar.attachToSprite(mine_button, -8, 0)
 statusbar.positionDirection(CollisionDirection.Bottom)
@@ -83,7 +89,6 @@ statusbar.value = 0
 statusbar.setColor(5, 11)
 statusbar.max = 100
 let statusbar2 = statusbars.create(30, 3, StatusBarKind.ItemQuantity)
-statusbar.setColor(5, 11)
 statusbar2.attachToSprite(mySprite2, -1, 0)
 statusbar2.value = 0
 statusbar2.positionDirection(CollisionDirection.Bottom)
@@ -127,26 +132,8 @@ forever(function () {
             ................................
             `],
         100,
-        true
+        false
         )
-        if (controller.A.isPressed()) {
-            for (let index = 0; index < 100; index++) {
-                pause(10)
-                statusbar.value += 1
-            }
-            if (list._pickRandom() == 0) {
-                statusbar2.value += 1
-            } else if (list._pickRandom() == 1) {
-                statusbar2.value += 1
-            } else if (list._pickRandom() == 2) {
-                statusbar2.value += 1
-            } else if (list._pickRandom() == 3) {
-                statusbar.value += 1
-            } else if (list._pickRandom() == 4) {
-                statusbar.value += 1
-            }
-            statusbar.value = 0
-        }
     } else {
         animation.runImageAnimation(
         mine_button,
@@ -187,5 +174,31 @@ forever(function () {
         100,
         true
         )
+    }
+})
+forever(function () {
+    if (mySprite.overlapsWith(mine_button)) {
+        if (controller.A.isPressed()) {
+            for (let index = 0; index < 100; index++) {
+                pause(10)
+                statusbar.value += 1
+            }
+            if (list._pickRandom() == 0) {
+                statusbar2.value += 1
+            } else if (list._pickRandom() == 1) {
+                statusbar2.value += 1
+            } else if (list._pickRandom() == 2) {
+                statusbar2.value += 1
+            } else if (list._pickRandom() == 3) {
+                statusbar2.value += 1
+            } else if (list._pickRandom() == 4) {
+                statusbar2.value += 1
+            } else {
+            	
+            }
+            statusbar.value = 0
+        }
+    } else {
+    	
     }
 })
